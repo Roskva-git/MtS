b6fe50c3a85d2b5cc568fed3439e78c8f19846af
Roskva-git <rosktb@gmail.com>
2025-12-18 15:12:30 +0100

    Refactor comments and section numbers in preprocessing script
    Updated comments and section numbering for clarity.

--- code/EEG/Preprocessing/1_TST_batch_preprocessing_pipeline.m
+++ code/EEG/Preprocessing/1_TST_batch_preprocessing_pipeline.m
@@ -1,8 +1,14 @@
-%% Batch EEG Preprocessing Pipeline for TST Data with ICA
+%% 1. Batch EEG Preprocessing Pipeline for TST Data with ICA
 
 % Author: Røskva
 % Date: 09.05.25
-% Last changed: 16.12.25
+% Last changed: 18.12.25
+
+% Workflow: 
+% Step 1. Run this preprocessing script with ICA
+% step 2. Reject independent components 
+% Step 3. Run the mapping script using the logfile to map events
+% Step 4. Run epoching script
 
 % DESCRIPTION:
 % This script performs batch preprocessing of EEG data (.vhdr files) for 
@@ -14,17 +20,16 @@
 % It extracts a base name for each file for import and storage.
 % We apply a low pass filter (40Hz), resample to 500Hz, high pass filter (0.1Hz) and remove EMG channels.
 % We map electrode names and add FCz as the empty reference eletrode, then we re-reference to the average of the earlobes.
-% We create an event list and rename the triggers for epoching (different script).
 % We run the ICA but do not remove any components.
 
 
 %% 0. Initialize EEGLAB and Set Paths
-addpath('\\hume.uio.no\student-u87\....matlab-link', ...
+addpath('\\hypatia.uio.no\lh-sv-psi\MICC\Projects\MindTheSleep\Code\tools\eeglab2025.0.0', ...
     '\\hypatia.uio.no\lh-sv-psi\project-folder', ...
     '\\hypatia.uio.no\lh-sv-psi\project-code-folder');
 eeglab;  % Start EEGLAB
 % Define folders
-raw_folder = '\\hypatia.uio.no\raw-folder';
+raw_folder = '\\hypatia.uio.no\raw-folder\';
 out_folder = '\\hypatia.uio.no\out-folder';
 % Get all .vhdr files in the raw folder
 vhdr_files = dir(fullfile(raw_folder, '*.vhdr'));
@@ -47,54 +52,17 @@ for f = 1:length(vhdr_files)
         EEG = pop_select(EEG, 'rmchannel', {'L_EMG', 'R_EMG'});
 
 
-        %% Look up locations + append empty FCz
+        %% 5. Look up locations + append empty FCz
         
 EEG = pop_chanedit(EEG, {'lookup','standard_1005.elc'},'append',1,'changefield',{2,'labels','FCz'});
 
 
-        %% Re-reference to avg earlobes + add online reference
+        %% 6. Re-reference to avg earlobes + add online reference
         EEG = pop_reref( EEG, find(ismember({EEG.chanlocs.labels}, {'A1','A2'})),'refloc',struct('labels',{'FCz'},'type',{''},'theta',[],'radius',[],'X',[],'Y',[],'Z',[],'sph_theta',[],'sph_phi',[],'sph_radius',[],'urchan',[],'ref',{''},'datachan',0));
-
-%% 7. Event List Creation and Trigger Renaming
-        % Relevant triggers
-        gl = 'S  1'; % Go stimulus - left - go trial
-        gr = 'S  2'; % Go stimulus - right - go trial
-        gsl = 'S  3'; % Go stimulus - left - stop trial
-        gsr = 'S  4'; % Go stimulus - right - stop trial
-        sl = 'S  5'; % Stop stimulus - left
-        sr = 'S  6'; % Stop stimulus - right
-        rl = 'S 65';  % Response - left
-        rr = 'S 71'; % Response - right
-        cl = 'Not collected'; % for CRT task, not collected in this data set
-        cr = 'Not collected';
-        % Extract meaningful event names
-        EEG = SST_recode(EEG, EEG.srate, gl, gr, gsl, gsr, sl, sr, rl, rr, cl, cr);
-
-%% 7.1 Event list creation and Trigger renaming. Event types double checked with EEG.event (not the recoding)
-
-% the event types in the EEG file are :
-% 'S  1' = newblock. Marks the time of a new block
-% 'S  2' = stimulus one. Marks when stimulus one is displayed
-% 'S  3' = stimulus two. Marks when stimulus two is displayed
-% 'S  4' = error. Marks time of an incorrect response
-% 'S  5' = correct. Marks time of a correct response 
-% 'S254' = trigger in the beginning of block to align EEG
-% 'boundary' = ?
-
-% IMPORTANT: THERE'S 2 SPACES BETWEEN 'S' AND THE 'NUMBER' IN EEG FILE TRIGGERS. 
-
-% Logic and descriptons of recoding
-% 'R/S'             - repeat or switch trial 
-% 'cC/cE'           - current correct response or current error response
-% 'pC/pE'           - previous correct response or previous error response
-% 'Stim1/Stim2'     - marks when stimulus one/two is displayed
-% 'stim/res'        - moment of stimulus presentation or moment of response giving
-% 'Firsttrial'      - first trial of a new block 
-
         
-%% 8. Run ICA
+%% 7. Run ICA
         EEG = pop_runica(EEG, 'extended', 1, 'interrupt', 'on');
-%% 9. Save ICA Decomposition
+%% 8. Save ICA Decomposition
         out_filename = [base_name '_ICA.set'];
         pop_saveset(EEG, 'filename', out_filename, 'filepath', out_folder);
         disp(['SUCCESS: ' base_name ' processed and saved as ' out_filename]);
